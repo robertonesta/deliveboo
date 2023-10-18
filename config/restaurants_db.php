81c789f97795cd2b6e61a8439b7c1dfdb11f4564
RobertoNesta <robertonesta1993@gmail.com>
2023-10-18 11:02:55 +0200

    adding a restaurants_db file in the config folder

---
+++ config/restaurants_db.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+
+    [
+        "name" => "Il panificio di Crosta",
+        "address" => "Via Felice Bellotti 13, Milano, 20129" ,
+        "piva" => "03061930800",
+        "photo" => "https://static.gamberorosso.it/2019/07/pane-di-crosta-milano.jpg",
+    ],
+    [
+        "name" => "Pizzeria da Gianni al Mattone",
+        "address" => "Via dei Monti di Primavalle 169, Roma, 00168" ,
+        "piva" => "03061930810",
+        "photo" => "https://media-cdn.tripadvisor.com/media/photo-s/2a/58/e9/f0/caption.jpg",
+    ],
+    [
+        "name" => "Panificio SANNA dal 1976",
+        "address" => "Via Marghera 37, Milano, 20149" ,
+        "piva" => "03061930821",
+        "photo" => "https://sannamilano.it/wp-content/uploads/2022/08/Unknown.jpg",
+    ],
+    [
+        "name" => "Panificio Marghera Milano",
+        "address" => "Via Marghera 51, Milano, 20149" ,
+        "piva" => "03061930845",
+        "photo" => "https://s3-media0.fl.yelpcdn.com/bphoto/UtLuEgcEbiyBZAW8BPTHHw/348s.jpg",
+    ],
+    [
+        "name" => "All' Antico Vinaio",
+        "address" => "Via dei Neri 65r, Firenze, 50122" ,
+        "piva" => "030619308074",
+        "photo" => "https://www.toscanainfesta.eu/media/site/immagini/schede/loghi/medium/10345-allantico-vinaio.jpg",
+    ],
+    [
+        "name" => "Bella Torino",
+        "address" => "Via S. Quintino 15, Torino, 10121" ,
+        "piva" => "45678912354",
+        "photo" => "https://www.bellatorino.nl/wp-content/uploads/2016/06/pizzaria-ristorante-bella-torino-dorpsstraat-zoetermeer-gevel-1.jpg",
+    ],
+    [
+        "name" => "Yasumi",
+        "address" => "Via Roma 53, Rozzano, 20089 " ,
+        "piva" => "68542719320",
+        "photo" => "https://restaurantyasumi.nl/wp-content/uploads/2021/11/DSC_0408_1-scaled.jpg",
+    ],
+    [
+        "name" => "Pizzeria Giulia",
+        "address" => "Viale delle Magnolie 13, Palermo, 90144" ,
+        "piva" => "15248975843",
+        "photo" => "",
+    ],
+    [
+        "name" => "I Mangioni",
+        "address" => "Via dei Leoni 58, Firenze, 50122" ,
+        "piva" => "15042306518",
+        "photo" => "https://www.jmenu.it/media/cache/mangioni/page-menu/1500x900/menu-digitale-jmenu-gold-cutlery-set-on-black-background2-00996.jpg",
+    ],
+    [
+        "name" => "U panuozzo",
+        "address" => "Via degli Artisti 27, Firenze, 50132" ,
+        "piva" => "03215048175",
+        "photo" => "https://www.o-panuozzo.nl/wp-content/uploads/2018/04/3a3e705a-3118-4842-b86d-29f621b54061.jpg",
+    ],
+    [
+        "name" => "Glamour",
+        "address" => "Via Gustavo di Valdengo, 2, 13900 Biella BI" ,
+        "piva" => "12345678901",
+        "photo" => "https://www.discoverbiella.com/orangeCMS/repo/immagini/Screen_Shot_2016-11-18_at_10.0_a84b3957aa.png",
+    ],
+    [
+        "name" => "Cencio La parolaccia",
+        "address" => "Vicolo del Cinque, 3, 00153 Roma RM" ,
+        "piva" => "34523456789",
+        "photo" => "https://media-cdn.tripadvisor.com/media/photo-s/03/2d/ef/72/cencio-la-parolaccia.jpg",
+    ],
+    [
+        "name" => "Ristorante Riso Amaro",
+        "address" => "Italia, Viale Regina Margherita, 22, 04022 Fondi LT" ,
+        "piva" => "11523456789",
+        "photo" => "https://www.scattidigusto.it/wp-content/uploads/2019/07/riso-amaro-fondi-1280x854.jpg",
+    ],
+    [
+        "name" => "Ristorante Solferino",
+        "address" => "Piazza Solferino, 3/B, 10121 Torino TO" ,
+        "piva" => "21438347421",
+        "photo" => "https://ristorantesolferino.com/cms/wp-content/uploads/SOLFERINO-ESTERNO.jpg",
+    ],
+    [
+        "name" => "Ristorante La Galleria",
+        "address" => "Salita Ramirez, 2, 90134 Palermo PA" ,
+        "piva" => "67483746352",
+        "photo" => "https://img.restaurantguru.com/r8df-La-Galleria-interior-2021-08.jpg",
+    ]
+];

447242fa1260e91c45c09011f26175508e4664cd
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-10-19 12:46:29 +0200

    dashboard fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use App\Models\Restaurant;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view ('admin.dashboard');
+        $user = Auth::user(); 
+        $user_id = $user->id; 
+    
+        // Ora puoi usare $user_id nella tua query
+        $restaurants = Restaurant::where('user_id', $user_id)->paginate(5);
+        return view('admin.dashboard', compact('restaurants'));
     }
 }

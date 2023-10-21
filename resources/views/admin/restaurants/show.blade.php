6100e10e07493429a4df7c552d36fe20c5bd8add
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-10-21 18:54:20 +0200

    fix show restaurant view

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="card text-center" style="width: 18rem;">
-    @if (Str::contains($restaurant->photo, 'photo'))
-        <img src="{{ asset('storage/' . $restaurant->photo) }}" class="card-img-top" alt="...">
+<div class="row justify-content-center align-items-center">
+    <div class="col-8 vh-100 p-3">
+        @if (Str::contains($restaurant->photo, 'upload'))
+        <img src="{{ asset('storage/' . $restaurant->photo) }}" class="card-img-top h-100 fit-cover " alt="...">
     @else
-        <img class="card-img-top rounded" src="{{ asset('storage/' . $restaurant->photo) }}" alt="">
+        <img class="card-img-top rounded w-100" src="{{$restaurant->photo }}" alt="">
     @endif
-    <div class="card-body">
-        <h5 class="card-title">{{ $restaurant->name }}</h5>
-        <p class="card-text">{{ $restaurant->address }}</p>
-        <p class="card-text"><strong>Partita IVA: </strong>{{ $restaurant->piva }}</p>
-
-        <!-- Mostra le tipologie associate al ristorante -->
-        <p class="card-text"><strong>Tipologie:</strong>
+    </div>
+    <div class="col-4 p-3">
+        <div>
+            <h2 class="fs-1 ">{{ $restaurant->name }}</h2>
+        </div>
+        <div class="down">
+            <div class="py-3">
+                <h4>Indirizzo</h4>
+                <p class="fst-italic">{{ $restaurant->address }}</p>
+            </div>
+            <div class="py-3">
+                <h4>Partita IVA</h4>
+                <p class="fst-italic">{{ $restaurant->piva }}</p>
+            </div>
+            <div class="py-3">
+              <!-- Mostra le tipologie associate al ristorante -->
+            <p class="card-text fst-italic"><strong>Tipologie:</strong>
             @if($restaurant->typologies->count() > 0)
                 @foreach($restaurant->typologies as $typology)
                     {{ $typology->name }}
@@ -25,13 +36,13 @@
                 Nessuna tipologia associata
             @endif
         </p>
-        
-    </div>
-
-    <div class="card-body">
-        <a href="{{ route('admin.restaurants.edit', $restaurant) }}" class="card-link">Modifica</a>
+            </div>
+        </div>
     </div>
 </div>
+
+
+
 
 
 @endsection

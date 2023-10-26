a624878042f6261ff98bf299552664f4e025c685
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-10-26 10:39:37 +0200

    preview image in edit+create dishes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -6,30 +6,44 @@
    @include('partials.validation_error')
     <form action="{{route('admin.dishes.store')}}" autocomplete="off" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="mb-3 name">
+        <div class="mb-3 name" >
             <label for="name" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="name" name="name" aria-describedby="name" placeholder="Inserisci un nome per il piatto">
+            <input type="text" 
+                class="form-control" 
+                id="name" 
+                name="name"  
+                aria-describedby="name" 
+                placeholder="Inserisci un nome per il piatto"
+                value="{{ old('name') }}"> 
             @error('name')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
-        <div class="mb-3 photo">
-              <label for="photo" class="form-label">Immagine</label>
-              <input type="file" name="photo" id="photo" class="form-control @error('photo') is-invalid @enderror" />
-              @error('photo')
-              <small class="text-danger">{{ $message }}</small>
-              @enderror
+        <div class="mb-3 row align-items-center">
+            <div class="col-12" id="image">
+                <label for="photo" class="col-sm-2 col-form-label">Immagine</label>
+                <input type="file" name="photo" id="photo" class="form-control @error('photo') is-invalid @enderror" />
+                @error('photo')
+                <small class="text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="col-md-5" id="image-preview-container"></div>
         </div>
+            
+
+        
         <div class="mb-3 description">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="text" class="form-control" id="description" name="description" aria-describedby="description" placeholder="Descrivi il piatto" rows="4"></textarea>
+            <textarea class="form-control" id="description" name="description" aria-describedby="description" placeholder="Descrivi il piatto" rows="4"
+            >{{ old('description') }}</textarea>
             @error('description')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
+        
         <div class="mb-3 ingredients">
             <label for="ingredients" class="form-label">Ingredienti</label>
-            <textarea type="text" class="form-control" id="ingredients" name="ingredients" aria-describedby="ingredients" placeholder="Nomina gli ingredienti del piatto" rows="4"></textarea>
+            <textarea type="text" class="form-control" id="ingredients" name="ingredients" aria-describedby="ingredients" placeholder="Nomina gli ingredienti del piatto" rows="4">{{ old('ingredients') }}</textarea>
             @error('ingredients')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
@@ -53,4 +67,33 @@
         </div>
     </form>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const photoInput = document.getElementById('photo');
+        const previewContainer = document.getElementById('image-preview-container');
+        const resize = document.getElementById('image');
+
+        photoInput.addEventListener('change', function() {
+            const file = photoInput.files[0];
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                const imagePreview = document.createElement('img');
+                imagePreview.src = e.target.result;
+                imagePreview.className = 'img-fluid'; 
+                resize.className = 'col-md-7';
+                previewContainer.innerHTML = ''; 
+                previewContainer.appendChild(imagePreview);
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        });
+    });
+</script>
 @endsection
+
+
+
